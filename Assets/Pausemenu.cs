2ea3c72435c6ce1917148484f8b1d61e9a344327
Shourya Mittal <shouryam00@gmail.com>
2021-02-23 16:53:00 +0530

    UI added

---
+++ Assets/Pausemenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pausemenu : MonoBehaviour
+{
+    public static bool GameisPause = false;
+    public GameObject PauseMenuUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameisPause)
+            { Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameisPause = false;
+    }
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameisPause = true;
+    }
+}
